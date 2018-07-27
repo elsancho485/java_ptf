d9f3c2f16161ed1fd45a253b1dbdeba4ce44bf1a
Alexander <atkachenko@smedialink.com>
2018-07-27 13:35:58 +0300

    Создан автотес для модификации группы без редкордера(почти)

---
+++ addressbook-web-test/src/test/java/ru/stqa/ptf/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.ptf.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.ptf.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase{
+
+  @Test
+  public void testGroupModification() {
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("yest1", "test2", "test3"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+}
