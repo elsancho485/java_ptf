1a9bd2fbffdfac2deef48cd2e1683a4bccaea82f
Alexander <scotty-scater@mail.ru>
2018-08-24 16:44:01 +0300

    Добавили сортировку коллекции в тесты создания и модификации группы
    С помощью лямбда-выражений

--- addressbook-web-test/src/test/java/ru/stqa/ptf/addressbook/tests/CreationGroupTest.java
+++ addressbook-web-test/src/test/java/ru/stqa/ptf/addressbook/tests/CreationGroupTest.java
@@ -1,6 +1,5 @@
 package ru.stqa.ptf.addressbook.tests;
 
-
 import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.ptf.addressbook.model.GroupData;
@@ -24,8 +23,10 @@ public class CreationGroupTest extends TestBase {
     Assert.assertEquals(after.size(), before.size() + 1);
 
     before.add(group);
-    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
-    Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before , after);
   }
 
 }
