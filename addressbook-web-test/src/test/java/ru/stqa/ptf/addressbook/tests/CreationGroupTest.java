7398735fa55c4626c32a834983c3868a1386c29d
Alexander <scotty-scater@mail.ru>
2018-08-24 16:21:52 +0300

    Добавили лямба-выражение в тест создания группы

--- addressbook-web-test/src/test/java/ru/stqa/ptf/addressbook/tests/CreationGroupTest.java
+++ addressbook-web-test/src/test/java/ru/stqa/ptf/addressbook/tests/CreationGroupTest.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.ptf.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -14,7 +15,7 @@ public class CreationGroupTest extends TestBase {
   public void testCreationGroup() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    GroupData group = new GroupData("yest1", "test2", "test3");
+    GroupData group = new GroupData("test1", "test2", "test3");
     app.getGroupHelper().initGroupCreation();
     app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupCreation();
@@ -22,15 +23,8 @@ public class CreationGroupTest extends TestBase {
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-
-    int max = 0;
-    for (GroupData g : after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
     before.add(group);
-    group.setId(max);
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
   }
 
