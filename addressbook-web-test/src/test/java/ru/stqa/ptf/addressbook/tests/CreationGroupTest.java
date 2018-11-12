2634678cd0f287c84dc2d8f9e10a1939665a59ac
Alexander <scotty-scater@mail.ru>
2018-11-12 16:39:59 +0300

    Дабавили настройку параметров тестовых методов
    Добавили настройку тестовых методов с помощью @DataProvider

--- addressbook-web-test/src/test/java/ru/stqa/ptf/addressbook/tests/CreationGroupTest.java
+++ addressbook-web-test/src/test/java/ru/stqa/ptf/addressbook/tests/CreationGroupTest.java
@@ -5,20 +5,32 @@ import ru.stqa.ptf.addressbook.model.GroupData;
 import ru.stqa.ptf.addressbook.model.Groups;
 
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.*;
 
 public class CreationGroupTest extends TestBase {
 
-  @Test
-  public void testCreationGroup() {
+  @DataProvider
+  public Iterator<Object[]> validGroups() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new GroupData().withName("test1").withHeader("header 1").withFooter("footer 1")});
+    list.add(new Object[] {new GroupData().withName("test2").withHeader("header 2").withFooter("footer 2")});
+    list.add(new Object[] {new GroupData().withName("test3").withHeader("header 3").withFooter("footer 3")});
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validGroups")
+  public void testCreationGroup(GroupData group) {
     app.goTo().groupPage();
     Groups before = app.group().all();
-    GroupData group = new GroupData().withName("Test1").withHeader("Test2").withFooter("Test3");
     app.group().create(group);
+    assertThat(app.group().count(), equalTo(before.size() + 1));
     Groups after = app.group().all();
-    assertThat(after.size(), equalTo(before.size() + 1));
     assertThat(after, equalTo(
             before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
