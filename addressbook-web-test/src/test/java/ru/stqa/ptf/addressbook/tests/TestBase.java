fcbe2a8632aeac620b45152d298fbb50c6b75aca
Alexander <scotty-scater@mail.ru>
2018-11-13 16:07:26 +0300

    Добавлено протоколирование
    Добавили протоколирование(логи) с помощью Logback. Как для консоли, так и с созданием лог файлов

--- addressbook-web-test/src/test/java/ru/stqa/ptf/addressbook/tests/TestBase.java
+++ addressbook-web-test/src/test/java/ru/stqa/ptf/addressbook/tests/TestBase.java
@@ -1,13 +1,19 @@
 package ru.stqa.ptf.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.*;
 import ru.stqa.ptf.addressbook.appmanager.ApplicationManager;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 import static org.testng.Assert.fail;
 
 public class TestBase {
+
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
 
   protected static final ApplicationManager app
           = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
@@ -24,6 +30,16 @@ public class TestBase {
     if (!"".equals(verificationErrorString)) {
       fail(verificationErrorString);
     }
+  }
+
+  @BeforeMethod(alwaysRun = true)
+  public void logTestStart(Method m, Object[] p) {
+    logger.info("Start test " + m.getName() + " with parameteres " + Arrays.asList(p));
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public  void  logTestStop(Method m) {
+    logger.info("Stop test " + m.getName());
   }
 
 }
